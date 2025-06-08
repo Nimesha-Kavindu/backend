ca7f286a2202b060cbb6ab146f9356d50f013aca
Nimesha Kavindu <fc115568@foc.sjp.ac.lk>
2025-06-08 17:22:39 +0530

    password hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,15 @@
 import User from "../models/users.js";
+import bcrypt from "bcrypt";
 
 export function createUser(req, res) {
+
+    const hashPassword = bcrypt.hashSync(req.body.password, 10);
+
     const user = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        password: req.body.password,
+        password: hashPassword,
         role: req.body.role,
     });
 
@@ -28,4 +32,36 @@ export function getUsers(req, res) {
             message: 'Error fetching users'
         });
     });
+}
+
+export function loginUser(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email: email}).then((user) => {
+        if (!user) {
+            return res.status(404).json({
+                message: 'User not found'
+            });
+        }
+
+        const isPasswordValid = bcrypt.compareSync(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({
+                message: 'Invalid password'
+            });
+        }
+
+        res.json({
+            message: 'Login successful',
+            user: {
+                id: user._id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+                role: user.role,
+                img: user.img
+            }
+        });
+    })
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createUser, getUsers } from '../controllers/userController.js';
+import { createUser, getUsers, loginUser } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/', createUser);
 userRouter.get('/', getUsers)
+userRouter.post('/login', loginUser);
 
 export default userRouter;
