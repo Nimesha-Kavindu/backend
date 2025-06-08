00b275f8ec589e12c834e10b272908d728d918d5
Nimesha Kavindu <fc115568@foc.sjp.ac.lk>
2025-06-08 10:52:09 +0530

    add bodyparse

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.json({
         message: 'hi, this is a simple express server',
     });
+
+    console.log(req.body.name);
 })
 
 app.listen(3000, () => {
