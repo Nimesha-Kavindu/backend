7a5018b03172714939d08d726003f39e20797daf
Nimesha Kavindu <fc115568@foc.sjp.ac.lk>
2025-06-08 11:22:59 +0530

    add changes

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
 
 const app = express();
 app.use(bodyParser.json());
+
+mongoose.connect("mongodb+srv://Nimesha:nimesha@cluster0.mshl9m6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(
+    () => {
+        console.log('Connected to MongoDB');
+    }
+).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
 
 app.get('/', (req, res) => {
     res.json({
@@ -15,3 +26,5 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
+
+//mongodb+srv://Nimesha:nimesha@cluster0.mshl9m6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
