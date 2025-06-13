6e7a23b87fbe09894a8fc73d08f118506d91eeab
Nimesha Kavindu <fc115568@foc.sjp.ac.lk>
2025-06-13 20:08:48 +0530

    add

--- controllers/userController.js
+++ controllers/userController.js
@@ -76,6 +76,9 @@ export function loginUser(req, res) {
                 res.json({
                     message: 'Login successful',
                     token: token,
+                    role : user.role,
+
+
                 });
             } else {
                 res.status(401).json({
